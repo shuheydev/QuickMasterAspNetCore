a68460075f142be4167ef198125b9919f4943a5a
shuhey <nishizawa@timedia.co.jp>
2019-05-16 11:56:34 +0900

    プロジェクト ファイルを追加します。

---
+++ QuickMaster/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickMaster.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello, world");
+        }
+
+        public IActionResult Greet()
+        {
+            ViewBag.Message = "Hello,World!";
+
+            //テンプレートを呼び出す
+            return View();
+        }
+    }
+}
